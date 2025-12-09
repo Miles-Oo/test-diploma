b2c3488d029c6c197fb7737bb0a0ab54ccc9123e
NaczoZombi <adrian.kala@icloud.com>
2025-12-09 18:04:06 +0100

    ruch gracza
    czemu nowy movement jest taki dziwny....

---
+++ Assets/GRA/gracz/skrypty/movement/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    [SerializeField] private InputAction moveAction;
+    private float moveSpeed = 2f;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+    }
+
+    private void Update()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = moveInput * moveSpeed;
+    }
+}
